402257aefdd17441b5912ac69a6b8e8f1ccca9dc
geeky-auro <aurosaswat.apk@gmail.com>
2024-08-26 18:11:21 +0530

    Added Custom Hooks

---
+++ 06currencyconverter/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/hooks/**/*.{js,ts,jsx,tsx,mdx}",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
